c0aa2577bf8a4cd12106e3f28993bb1bc0c2c0bc
jivinjules <juliehodges05@gmail.com>
2018-11-06 21:53:37 -0600

    image

--- client/src/containers/Info/info.js
+++ client/src/containers/Info/info.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Title from '../../components/Title/title'
+import image from '../../components/Images/pillbox.png'
 
 class Info extends React.Component {
     render() {
         return (
             <div>
                 <Title />
+                <div className='col-md-12' id='image'>
+                    <img src={image} alt="pillbox" />
+                </div>
                 <div className='bbstyle'>
                     <div className='row'>
                         <div className='col-md-12'>
-                            <img src='../client/public/pillbox.png' alt="pillbox" />
+
                             <p>Pillbox was born out of the need to...</p>
                         </div>
                     </div>
