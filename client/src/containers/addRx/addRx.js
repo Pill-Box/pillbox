3709da4e9b79f564f705133cb53eb4c6c6ab426c
jivinjules <juliehodges05@gmail.com>
2018-11-13 20:20:37 -0600

    stuff

---
+++ client/src/containers/addRx/addRx.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TabScreens from '../../components/Sidebar/bottomBar'
+
+class AddRx extends React.Component {
+    render() {
+        return (
+            <TabScreens />
+        )
+    }
+}
+
+export default AddRx
