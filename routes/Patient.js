3f0a8a8b7a363b1f1942176c34fc2a4214703afa
jpeek01 <jpeek01@gmail.com>
2018-11-19 21:52:25 -0600

    updates to routes

--- routes/Patient.js
+++ routes/Patient.js
@@ -1,8 +1,29 @@
 const db = require('../models')
-
-//Get all patients by user id
 module.exports = app => {
-  app.get("/api/patients/:id", (req, res) => {
+//New User
+app.post('/api/users', (req, res) => {
+  db.User.create(req.body).then(data => {
+    res.json(data)
+  })
+})
+
+//New Patient route
+  app.post('/api/patients', (req, res) => {
+    db.Patient.create(req.body).then(data => {
+      res.json(data)
+    })
+  })
+
+//New Rx
+app.post('/api/Rx', (req, res) => {
+  db.Rx.create(req.body).then(data => {
+    res.json(data)
+  })
+})
+
+// List of patients
+//Get all patients by user id
+  app.get("/api/user/:id", (req, res) => {
     db.User.findOne({
       where: {
         id: req.params.id
@@ -15,6 +36,7 @@ module.exports = app => {
     });
   })
 
+//List of Patients and their Rxs
 //Get all patients and rxs by user id
   app.get("/api/user/patient/rx/:id", (req, res) => {
     db.User.findOne({
@@ -32,20 +54,19 @@ module.exports = app => {
     });
   })
 
+//Get a list of Patients Rxs by patient id
 
-  app.get("/patients", (req, res) => {
-    db.Patient.find({
-      include: [db.User]
-    }).then = (dbPatient) => {
-      res.json(dbPatient);
-    };
+  app.get("/api/user/patient/:patientId", (req, res) => {
+    db.Patient.findOne({
+      where: {
+        id: req.params.patientId
+      },
+      include: [{
+        model: db.Rx
+      }]
+    }).then(data => {
+      res.json(data);
+    });
   })
 
-  //New Patient route
-  app.post('/api/patients', (req, res) => {
-    db.Patient.create(req.body).then(data => {
-      res.json(data)
-    })
-
-  })
 }
