8ac7f7dc83fb09e3b4e852f6403a9d0ee402d4c9
alex-bowen <alexbowen2017@gmail.com>
2018-11-26 19:37:18 -0600

    Minor, yet crucial changes

--- client/src/components/Sidebar/bottomBar.js
+++ client/src/components/Sidebar/bottomBar.js
@@ -7,19 +7,16 @@ class TabScreens extends React.Component {
         return (
            <div className='bottomNav'>
                 <div className='row'>
-                    <div className='col-md-2 bar-cols'>
+                    <div className='col-md-3 bar-cols'>
                         <a href='/dashboard'><i className="fas fa-home"></i></a>
                     </div>
                     <div className='col-md-3 bar-cols'>
                         <a href='/addRx'><i className="fas fa-prescription-bottle-alt"></i></a>
                     </div>
-                    <div className='col-md-2 bar-cols'>
+                    <div className='col-md-3 bar-cols'>
                         <a href='/addpatient'><i className="fas fa-users"></i></a>
                     </div>
                     <div className='col-md-3 bar-cols'>
-                        <a href='/calendar'><i className="far fa-calendar-alt"></i></a>
-                    </div>
-                    <div className='col-md-2 bar-cols'>
                         <a href='/signout'><i className="fas fa-sign-out-alt"></i></a>
                     </div>
                 </div>
