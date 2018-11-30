ca684bbe891f2f85077ebdd0eb2f5446eacd44d7
alex-bowen <alexbowen2017@gmail.com>
2018-11-29 21:05:42 -0600

    Complete

--- client/src/containers/Dashboard/dashboard.js
+++ client/src/containers/Dashboard/dashboard.js
@@ -131,7 +131,7 @@ class Dashboard extends React.Component {
                                         Pharmacy Number: {drug.pharmacy_number}<br />
                                         Notes: {drug.notes}<br />
                                      
-                                        <button onClick={() => this.getDrugInfo(drug.ndc)}>Get Drug Info</button>
+                                        <button className="get-drug-info" onClick={() => this.getDrugInfo(drug.ndc)}><i className="fas fa-notes-medical"></i>  Get Drug Info</button>
                                     </Rx>
                                 ))}
                             </RxModal>
